b1173c4e74eeeac1922a9963c061f614353ede44
Taz Jackson <tatjsn@users.noreply.github.com>
2022-09-19 13:45:26 +0100

    Introduce SPA

--- main.js
+++ main.js
@@ -1,23 +1,53 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import { elementOpen, elementClose, text, patch } from 'incremental-dom';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function getPropsArray(node) {
+  const result = [];
+  for (const attr of node.attributes) {
+    result.push(attr.name);
+    result.push(attr.value);
+  }
+  return result;
+}
 
-setupCounter(document.querySelector('#counter'))
+function traverse(node, top = false) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    text(node.data);
+    return;
+  }
+  if (!top) {
+    elementOpen(node.tagName.toLowerCase(), null, null, ...getPropsArray(node));
+  }
+  for (const child of node.childNodes) {
+    traverse(child);
+  }
+  if (!top) {
+    elementClose(node.tagName.toLowerCase());
+  }
+}
+
+document.body.addEventListener('click', async (e) => {
+  const origin = e.target.closest('a');
+
+  if (origin) {
+    const newHref = origin.href;
+
+    if (new URL(newHref).origin !== location.origin) {
+      return;
+    }
+
+    e.preventDefault();
+
+    const result = await fetch(newHref);
+    const text = await result.text();
+    const tmpl = document.createElement('template');
+    tmpl.innerHTML = text;
+
+    const newApp = tmpl.content.querySelector('#app');
+
+    patch(document.querySelector('#app'), () => {
+      traverse(newApp, true);
+    });
+
+    history.pushState({}, null, newHref);
+  }
+})
