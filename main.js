c0d01f08688ad506d445a25474d748a9a5b86fdb
Taz Jackson <tatjsn@users.noreply.github.com>
2022-09-19 14:59:36 +0100

    Support history back/forward

--- main.js
+++ main.js
@@ -25,29 +25,47 @@ function traverse(node, top = false) {
   }
 }
 
+function applyHtmlText(text) {
+  const tmpl = document.createElement('template');
+  tmpl.innerHTML = text;
+
+  const newApp = tmpl.content.querySelector('#app');
+
+  patch(document.querySelector('#app'), () => {
+    traverse(newApp, true);
+  });
+}
+
 document.body.addEventListener('click', async (e) => {
   const origin = e.target.closest('a');
 
-  if (origin) {
-    const newHref = origin.href;
-
-    if (new URL(newHref).origin !== location.origin) {
-      return;
-    }
-
-    e.preventDefault();
-
-    const result = await fetch(newHref);
-    const text = await result.text();
-    const tmpl = document.createElement('template');
-    tmpl.innerHTML = text;
-
-    const newApp = tmpl.content.querySelector('#app');
-
-    patch(document.querySelector('#app'), () => {
-      traverse(newApp, true);
-    });
-
-    history.pushState({}, null, newHref);
+  if (!origin) {
+    return;
   }
-})
+  const newHref = origin.href;
+
+  if (new URL(newHref).origin !== location.origin) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const result = await fetch(newHref);
+  const htmlText = await result.text();
+
+  applyHtmlText(htmlText);
+
+
+  history.pushState({ htmlText }, null, newHref);
+});
+
+window.addEventListener('popstate', (event) => {
+  if (!event.state || !event.state.htmlText) {
+    return;
+  }
+
+  applyHtmlText(event.state.htmlText);
+});
+
+// For history back
+history.replaceState({ htmlText: document.documentElement.outerHTML }, null, location);
